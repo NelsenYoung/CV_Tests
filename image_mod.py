132ff967e885e4ee560de806b5a9e6649e1d70bb
Nelsen Young <njyouny@ucdavis.edu>
2025-11-10 12:26:30 -0800

    max pooling layer added

--- image_mod.py
+++ image_mod.py
@@ -2,37 +2,62 @@ from PIL import Image
 import numpy as np
 import math
 
+def convole(img, kernel, stride, padding, convolution_func):
+    # Basic information gathering
+    h, w = img.shape
+    kernel_size = len(kernel)
+    print(img.shape)
+    # Set the output size
+    output = np.array([])
+    output_h = (h + 2 * padding - kernel_size) // stride + 1
+    output_w = (w + 2 * padding - kernel_size) // stride + 1
+    output = np.zeros((output_h, output_w))
+
+    # Convolution
+    for i in range(0, h - (kernel_size - 1), stride):
+        for j in range(0, w - (kernel_size - 1), stride):
+            window = img[i:i+kernel_size, j:j+kernel_size]
+            new_pixel = convolution_func(window, kernel)
+            output[i // stride, j // stride] = new_pixel
+    
+    return output
+
+def convolution_calc(window, kernel):
+    new_pixel = np.sum(window * kernel)
+    if new_pixel < 0:
+        new_pixel = 0
+    return new_pixel
+
+def max_pooling_calc(window, kernel):
+    new_pixel = np.max(window)
+    return new_pixel
+
 img = Image.open("pixel_mod_original.jpeg")
 img = img.convert('L')
-
 pixels = np.array(img)
-
-kernel = np.array([[-1, -1, -1],
+kernel = np.array([[-1, -2, -1],
                     [0, 0, 0],
-                    [1, 1, 1]])
-print(kernel.shape)
+                    [1, 2, 1]])
+channels = []
 
-output = np.array([])
+output = convole(pixels, kernel, 1, 0, convolution_calc)
+channels.append(output)
 
-h, w = img.size
-blur_rate = 9
-index = int(math.sqrt(blur_rate))
+kernel = np.array([[-1, 0, 1],
+                    [-2, 0, 2],
+                    [-1, 0, 1]])
+output = convole(pixels, kernel, 1, 0, convolution_calc)
+channels.append(output)
 
-output_h = h - index + 1
-output_w = w - index + 1
-output = np.zeros((output_h, output_w))
+pooling_channels = []
+# POOLING LAYER
+for channel in channels:
+    kernel = np.array([[0, 0],
+                       [0, 0]])
+    output = convole(channel, kernel, 2, 0, max_pooling_calc)
+    pooling_channels.append(output)
 
-for i in range(h):
-    cur_row = np.array([])
-    for j in range(w):
-        window = pixels[i:i+index, j:j+index]
-        if len(window) < index or len(window[0]) < index:
-            continue
-        new_pixel = np.sum(window * kernel)
-        new_pixel = abs(int(new_pixel))
-        output[i, j] = new_pixel
-
-print(output)
+print(output.shape)
 blurred_img = Image.fromarray(output).convert('RGB')
 blurred_img.save("blurred_image.jpg")
 
